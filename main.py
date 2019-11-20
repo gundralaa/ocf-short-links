8a2db46fc89a0550fbc7c9f5ddf88be349596421
gundralaa <abhinav.gundrala@outlook.com>
2019-11-19 17:09:05 -0800

    UI design

--- main.py
+++ main.py
@@ -2,34 +2,41 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import configparser
-from ocflib.misc.shorturls import add_shorturl
-from ocflib.misc.shorturls import delete_shorturl
-from ocflib.misc.shorturls import get_connection as shorturl_db
-from ocflib.misc.shorturls import get_shorturl
-from ocflib.misc.shorturls import rename_shorturl
-from ocflib.misc.shorturls import replace_shorturl
+#from ocflib.misc.shorturls import add_shorturl
+#from ocflib.misc.shorturls import delete_shorturl
+#from ocflib.misc.shorturls import get_connection as shorturl_db
+#from ocflib.misc.shorturls import get_shorturl
+#from ocflib.misc.shorturls import rename_shorturl
+#from ocflib.misc.shorturls import replace_shorturl
 app = Flask(__name__)
 
 @app.route('/')
 def render_page():
-    return render_template('page.html', success=None)
+    return render_template('page.html', success=None, search_results=[])
 
 @app.route('/', methods=['POST'])
 def add_url():
     original_url = request.form['urlInput']
     new_name = request.form['nameInput']
     config = configparser.ConfigParser()
-    path = 'CONFIG FILE PATH'
+    path = '../pass.conf'
     config.read(path)
     password = config['mysql']['password']
 
     if len(new_name) > 100:
         return render_template('page.html', success='New name too long')
 
-    with shorturl_db(user='ocfircbot', password=password) as ctx:
-        try:
-            add_shorturl(ctx, new_name, original_url)
-        except:
-            return render_template('page.html', success='Error')
-        else:
-            return render_template('page.html', success='Added')
+    #with shorturl_db(user='ocfircbot', password=password) as ctx:
+        #try:
+            #add_shorturl(ctx, new_name, original_url)
+        #except:
+            #return render_template('page.html', success='Error')
+        #else:
+            #return render_template('page.html', success='Added')
+
+    return render_template('page.html', success='Error', search_results=[])
+
+@app.route('/')
+def search_url():
+    search_results = [{'name' : 'Short Link', 'url' : 'https://www.ocf.berkeley.edu/s/name'}]
+    return render_template('page.html', success=None, search_results=search_results)
